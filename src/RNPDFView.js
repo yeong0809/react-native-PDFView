b9f191131e0d4b93397f96804c38022840254a1f
yeong0809 <31876283+yeong0809@users.noreply.github.com>
2022-08-14 18:11:04 +0800

    Update RNPDFView.js

--- src/RNPDFView.js
+++ src/RNPDFView.js
@@ -1,5 +1,6 @@
-import { requireNativeComponent, ViewPropTypes } from 'react-native';
+import { requireNativeComponent } from 'react-native';
 import PropTypes from 'prop-types';
+import {ViewPropTypes} from 'deprecated-react-native-prop-types';
 
 
 const componentInterface = {
